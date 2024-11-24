7cdb7cc8bc450ce0092c77097f368e8ed365497a
justJuste <jules.longin1@gmail.com>
2024-11-24 21:38:04 +0100

    add capitalizer

---
+++ Eval/Level 3/rstr_capitalizer.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlongin <jlongin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 20:55:44 by jlongin           #+#    #+#             */
+/*   Updated: 2024/11/24 21:35:17 by jlongin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+Assignment name  : rstr_capitalizer
+Expected files   : rstr_capitalizer.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes one or more strings and, for each argument, puts
+the last character that is a letter of each word in uppercase and the rest
+in lowercase, then displays the result followed by a \n.
+
+A word is a section of string delimited by spaces/tabs or the start/end of the
+string. If a word has a single letter, it must be capitalized.
+
+A letter is a character in the set [a-zA-Z]
+
+If there are no parameters, display \n.
+
+Examples:
+
+$> ./rstr_capitalizer | cat -e
+$
+$> ./rstr_capitalizer "a FiRSt LiTTlE TESt" | cat -e
+A firsT littlE tesT$
+$> ./rstr_capitalizer "SecONd teST A LITtle BiT   Moar comPLEX"
+"   But... This iS not THAT COMPLEX"
+"     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+seconD tesT A littlE biT   moaR compleX$
+   but... thiS iS noT thaT compleX$
+     okay, thiS iS thE lasT 1239809147801 buT noT    thE leasT    T$
+$>
+*/
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	gc;
+	int	i;
+
+	if (argc == 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	gc = 1;
+	while (gc < argc)
+	{
+		i = 0;
+		while (argv[gc][i])
+		{
+			if (argv[gc][i] >= 'A' && argv[gc][i] <= 'Z')
+				argv[gc][i] += 32;
+			if ((argv[gc][i] >= 'a' && argv[gc][i] <= 'z')
+				&& (argv[gc][i + 1] == ' '
+				|| argv[gc][i + 1] == '\t'
+				|| argv[gc][i + 1] == '\0'))
+				argv[gc][i] -= 32;
+			write(1, &argv[gc][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		gc++;
+	}
+	return (0);
+}
---
+++ Eval/Level 3/str_capitalizer.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlongin <jlongin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 20:08:55 by jlongin           #+#    #+#             */
+/*   Updated: 2024/11/24 21:00:39 by jlongin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+Assignment name  : str_capitalizer
+Expected files   : str_capitalizer.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes one or several strings and, for each argument,
+capitalizes the first character of each word (If it's a letter, obviously),
+puts the rest in lowercase, and displays the result on the standard output,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string. If a word only has one letter, it must be
+capitalized.
+
+If there are no arguments, the progam must display \n.
+
+Example:
+
+$> ./str_capitalizer | cat -e
+$
+$> ./str_capitalizer "a FiRSt LiTTlE TESt" | cat -e
+A First Little Test$
+$> ./str_capitalizer "__SecONd teST A LITtle BiT   Moar comPLEX"
+"   But... This iS not THAT COMPLEX"
+"     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+__second Test A Little Bit   Moar Complex$
+   But... This Is Not That Complex$
+     Okay, This Is The Last 1239809147801 But Not    The Least    T$
+$>
+*/
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	ac;
+	int	i;
+	int	count;
+
+	ac = 1;
+	if (argc == 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (ac < argc)
+	{
+		i = 0;
+		count = 1;
+		while (argv[ac][i])
+		{
+			if (argv[ac][i] == ' ' || argv[ac][i] == '\t')
+				count = 1;
+			if ((argv[ac][i] >= 'a' && argv[ac][i] <= 'z')
+				|| (argv[ac][i] >= 'A' && argv[ac][i] <= 'Z'))
+			{
+				if (count)
+				{
+					if (argv[ac][i] >= 'a' && argv[ac][i] <= 'z')
+						argv[ac][i] -= 32;
+					count = 0;
+				}
+				else if (argv[ac][i] >= 'A' && argv[ac][i] <= 'Z')
+					argv[ac][i] += 32;
+			}
+			write(1, &argv[ac][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		ac++;
+	}
+	return (0);
+}
