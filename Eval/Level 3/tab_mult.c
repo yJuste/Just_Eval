caf161dfa4cd66bf536b1255f66ddf7454825d14
justJuste <jules.longin1@gmail.com>
2024-11-25 09:39:02 +0100

    add tab mult

---
+++ Eval/Level 3/tab_mult.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tab_mult.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlongin <jlongin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:09:40 by jlongin           #+#    #+#             */
+/*   Updated: 2024/11/25 09:38:12 by jlongin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+Assignment name  : tab_mult
+Expected files   : tab_mult.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that displays a number's multiplication table.
+
+The parameter will always be a strictly positive number that fits in an int,
+and said number times 9 will also fit in an int.
+
+If there are no parameters, the program displays \n.
+
+Examples:
+
+$>./tab_mult 9
+1 x 9 = 9
+2 x 9 = 18
+3 x 9 = 27
+4 x 9 = 36
+5 x 9 = 45
+6 x 9 = 54
+7 x 9 = 63
+8 x 9 = 72
+9 x 9 = 81
+$>./tab_mult 19
+1 x 19 = 19
+2 x 19 = 38
+3 x 19 = 57
+4 x 19 = 76
+5 x 19 = 95
+6 x 19 = 114
+7 x 19 = 133
+8 x 19 = 152
+9 x 19 = 171
+$>
+$>./tab_mult | cat -e
+$
+$>
+*/
+#include <unistd.h>
+
+int	ft_atoi(char *s)
+{
+	int	res;
+
+	res = 0;
+	while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '+')
+		s++;
+	while (*s)
+		res = res * 10 + *s++ - '0';
+	return (res);
+}
+
+void	ft_putnbr(int n)
+{
+	char	digit;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	nb;
+
+	i = 1;
+	nb = 0;
+	if (argc == 2)
+	{
+		nb = ft_atoi(argv[1]);
+		while (i < 10 && nb <= 238609294)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nb);
+			write(1, " = ", 3);
+			ft_putnbr(i * nb);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
